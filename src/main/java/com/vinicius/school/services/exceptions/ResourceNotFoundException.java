d08b8b57087ad72e416e9b820a0306e18d65459e
vini <viniciusdemoraisrosa@gmail.com>
2023-06-01 13:16:38 -0300

    add exceptions

---
+++ src/main/java/com/vinicius/school/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vinicius.school.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
